0acaa15fef94b836ae6160353422fd8faf274dd6
Daniel Tome Fernandez <danieltomefer@gmail.com>
2020-03-26 18:58:52 +0100

    Stylelint works

--- stylelint.config.js
+++ stylelint.config.js
@@ -10,18 +10,7 @@ module.exports = {
     "no-eol-whitespace": null,
     "declaration-empty-line-before": null,
     "value-keyword-case": null,
-    "at-rule-no-unknown": [
-      true,
-      {
-        "ignoreAtRules": [
-          "tailwind",
-          "apply",
-          "responsive",
-          "variants",
-          "screen",
-          "use"
-        ]
-      }
-    ]
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true
   }
 }
